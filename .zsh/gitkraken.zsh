4517a57cba7529bcfbf2163a7f8574b4199a8b98
Chris <christian.daniel@live.de>
2023-02-15 23:49:51 +0100

    Remove gitkraken script

--- .zsh/gitkraken.zsh
+++
@@ -1,7 +0,0 @@
-# kraken () {
-# 	~/Applications/GitKraken.app/Contents/MacOS/GitKraken -p $(pwd)
-# }
-
-# If you want a git alias, add the following to your git config under [alias]
-# kraken = !"~/Applications/GitKraken.app/Contents/MacOS/GitKraken -p $(pwd)"
-# Now you can 'git kraken'!
