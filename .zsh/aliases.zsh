35edb5bf49d40da997ce3e3b36594d5db9fed2ba
Chris <christian.daniel@live.de>
2023-02-15 23:47:41 +0100

    Update aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,17 +1,3 @@
-# Alias
-# ---
-#
-alias k="kubectl"
-alias h="helm"
-alias tf="terraform"
-alias a="ansible"
-alias ap="ansible-playbook"
-alias dt="datree"
-
-# mac OS shortcuts
-alias code="open -a 'Visual Studio Code'"
-
-# ALIAS COMMANDS
 alias ls="exa --icons --group-directories-first -a"
 alias ll="exa --icons --group-directories-first -la"
 alias g="goto"
@@ -24,3 +10,9 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
+
+# git alias for dotfiles repo
+alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
+
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
--- .zsh/gitkraken.zsh
+++ .zsh/gitkraken.zsh
@@ -1,6 +1,6 @@
-kraken () {
-	~/Applications/GitKraken.app/Contents/MacOS/GitKraken -p $(pwd)
-}
+# kraken () {
+# 	~/Applications/GitKraken.app/Contents/MacOS/GitKraken -p $(pwd)
+# }
 
 # If you want a git alias, add the following to your git config under [alias]
 # kraken = !"~/Applications/GitKraken.app/Contents/MacOS/GitKraken -p $(pwd)"
