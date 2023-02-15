f6036615b997fe23ba969cac4caba10361c41d52
Chris <christian.daniel@live.de>
2023-02-15 23:58:41 +0100

    Add setup script

---
+++ setup-my-mac.sh
@@ -0,0 +1,31 @@
+brew install git
+brew install gh
+
+brew install starship
+brew install exa
+brew install z
+brew install wget
+brew install az
+brew install powershell
+brew install nvm
+brew tap microsoft/git
+brew install pyenv
+
+brew install --cask alt-tab
+brew install --cask bettertouchtool
+
+brew install --cask gitkraken
+brew install --cask docker
+brew install --cask rider
+brew install --cask visual-studio-code
+brew install --cask insomnia
+brew install --cask git-credential-manager-core
+brew install --cask github
+
+brew install --cask slack
+brew install --cask microsoft-teams
+
+defaults write com.apple.dock "autohide-delay" -float "0" && killall Dock
+defaults write com.apple.dock "autohide-time-modifier" -float "0.3" && killall Dock
+defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true" && killall Finder
+defaults write com.apple.screencapture type jpg
