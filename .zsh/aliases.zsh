5968049f96b2046150af8323b546b5dab67ff4fd
Chris <christian.daniel@live.de>
2023-02-15 22:33:37 +0100

    Add initial .zsh config

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,26 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias dt="datree"
+
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
+
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first -a"
+alias ll="exa --icons --group-directories-first -la"
+alias g="goto"
+alias grep='grep --color'
+alias os='open *.sln'
+alias x='exit'
+alias c='clear'
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
---
+++ .zsh/gitkraken.zsh
@@ -0,0 +1,7 @@
+kraken () {
+	~/Applications/GitKraken.app/Contents/MacOS/GitKraken -p $(pwd)
+}
+
+# If you want a git alias, add the following to your git config under [alias]
+# kraken = !"~/Applications/GitKraken.app/Contents/MacOS/GitKraken -p $(pwd)"
+# Now you can 'git kraken'!
