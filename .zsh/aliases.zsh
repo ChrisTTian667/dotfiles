451c47b1f9ff26721eb9cfca7e579020366f4594
Chris <christian.daniel@live.de>
2023-03-16 16:09:56 +0100

    Add some more aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -8,6 +8,10 @@ alias x="exit"
 alias c="clear"
 alias o="open ."
 
+alias brewup='brew update && brew upgrade && brew cleanup'
+alias reload='source ~/.zshrc'
+alias topcommands='history | awk "{CMD[\$2]++;count++;}END { for (a in CMD)print CMD[a] \" \" CMD[a]/count*100 \"% \" a;}" | grep -v "./" | column -c3 -s " " -t | sort -nr | nl | head -n10'
+
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
